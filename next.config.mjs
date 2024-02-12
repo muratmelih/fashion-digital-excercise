f1609abdc9304bd8640f6eab2533437f4bf28a91
muratmelih <muratmelihisik@gmail.com>
2024-02-12 20:25:41 +0300

    image domain fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['s3-eu-west-1.amazonaws.com']
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-eu-west-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
